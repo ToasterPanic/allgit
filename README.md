68d8ae80bc14c751233b3c9962db46531f49a6e8
Lukas <toasterpanic@gmail.com>
2023-05-02 23:42:07 +0000

    Update 5 files
    - /README.md
    - /index.js
    - /package.json
    - /public/index.html
    - /public/style.css

--- README.md
+++ README.md
@@ -8,27 +8,21 @@
 For Github:
 
 ```lua
-https://allgit.pp.ua/gh/user/repo@branch/file_path_here
+https://allgit.rk1.us/gh/user/repo@branch/file_path_here
 -- Get a file from a branch
-
-https://allgit.pp.ua/gh/user/repo/file_path_here
--- Get a file from the main branch
 ```
 
 For Gitlab:
 
 ```lua
-https://allgit.pp.ua/gl/user/repo@branch/file_path_here
+https://allgit.rk1.us/gl/user/repo@branch/file_path_here
 -- Get a file from a branch
-
-https://allgit.pp.ua/gl/user/repo/file_path_here
--- Get a file from the main branch
 ```
 
 **NOT ADDED YET, PLEASE WAIT AS WE ADD IT TO ALLGIT!** for Bitbucket:
 
 ```lua
-https://allgit.pp.ua/bb/user/repo/commit_sha_or_head/file_path_here
+https://allgit.rk1.us/bb/user/repo/commit_sha_or_head/file_path_here
 -- Get a file
 ```
 
---
+++ index.js
@@ -0,0 +1,72 @@
+var express = require("express");
+var app = express();
+const fetch = require('node-fetch');
+var MimeLookup = require('mime-lookup');
+var mime = new MimeLookup(require('mime-db'));
+
+var port = process.env.PORT || 3000;
+
+const approvedForHTML = [
+  "/gh/3kh0/3kh0-Assets@main/"
+]
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Allgit listening on port ${port}`)
+});
+
+app.get('/:service/:user/:repo@:branch/*', (req, res) => {
+  // Gets the file location
+  req.params.file = req.originalUrl.replace(/\/[^\/]+\/[^\/]+\/[^\/]+@[^\/]+\/([\s\S]+)/,"$1");
+  
+  // Checks which service you're using, and changes the url fetched accordingly
+  var url;
+  if (req.params.service == "gh") {
+    url = `https://raw.githubusercontent.com/${req.params.user}/${req.params.repo}/${req.params.branch}/${req.params.file}`;
+  } else if (req.params.service == "gl") {
+    url = `https://gitlab.com/${req.params.user}/${req.params.repo}/-/raw/${req.params.branch}/${req.params.file}`;
+  }
+  
+  console.log(url)
+  // Let's fetch!
+  fetch(url)
+  .then(res2 => {
+    if (res2.ok) {
+      // Response was ok, read the response and send it through!
+      res2.text().then(body => {
+        // Cache for 1 hour
+        res.setHeader('Cache-Control', "max-age=3600");
+        // Get correct MIME type, replace old MIME types with new ones
+        res.setHeader(
+          'Content-Type', 
+          (function() {
+            if (req.originalUrl.endsWith(".html")) {
+              console.log(req.originalUrl.match(/\/[^\/]+\/[^\/]+\/[^\/]+@[^\/]+\//)[0])
+              if (approvedForHTML.includes(req.originalUrl.match(/\/[^\/]+\/[^\/]+\/[^\/]+@[^\/]+\//)[0])) { 
+                return mime.lookup(url);
+              } else {
+                return "text/plain";
+              }
+            } else {
+              return mime.lookup(url)
+              .replace("application/javascript","text/javascript");
+            }
+          })()
+        );
+        res.end(body);
+      });
+    } else {
+      console.log(res2.status);
+      // Response was not ok, send an error status or message.
+      if (res2.status == "404") {
+        res.status(404).send("Sorry, we can't find that file.");
+      } else if (res2.status == "403") {
+        res.status(403).send("We couldn't get that file (repo doesn't exist?)");
+      } else {
+        res.status(500).send("There's been an error server-side and we couldn't fulfill your request.");
+      }
+    }
+    
+  })
+})
